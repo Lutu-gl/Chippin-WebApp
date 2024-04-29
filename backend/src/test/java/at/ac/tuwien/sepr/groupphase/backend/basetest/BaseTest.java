a86aab4a46418d603441939ea0a92b697d83efa0
Emil Hafner <e12014741@student.tuwien.ac.at>
2024-04-30 01:24:24 +0200

    Integration tests for user registration

---
+++ backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/basetest/BaseTest.java
@@ -0,0 +1,9 @@
+package at.ac.tuwien.sepr.groupphase.backend.basetest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class BaseTest {
+}
