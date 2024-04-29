1d06b0ba9ad5fe581181d8a5c0e8380372fb1962
Emil Hafner <e12014741@student.tuwien.ac.at>
2024-04-29 22:59:00 +0200

    Fix avoid star imports error

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/UserRegisterDto.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/UserRegisterDto.java
@@ -1,6 +1,9 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint.dto;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
