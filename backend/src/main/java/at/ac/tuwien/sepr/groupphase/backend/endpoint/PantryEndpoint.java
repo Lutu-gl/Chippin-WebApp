6333bf73613e9e833d8dfeab5ca0d097f0997576
Rafael Milchram <e12216445@student.tuwien.ac.at>
2024-05-03 17:58:38 +0200

    Fix security test fails due to missing secured annotation

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/PantryEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/PantryEndpoint.java
@@ -7,6 +7,7 @@ import at.ac.tuwien.sepr.groupphase.backend.service.PantryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,12 +29,14 @@ public class PantryEndpoint {
         this.itemMapper = itemMapper;
     }
 
+    @Secured("ROLE_USER")
     @GetMapping("/{pantryId}/pantry")
     public List<ItemDetailDto> findAllInPantry(@PathVariable long pantryId) {
         LOGGER.info("GET /api/v1/group/{}/pantry", pantryId);
         return itemMapper.listOfItemsToListOfItemDetailDto(pantryService.findAllItems(pantryId));
     }
 
+    @Secured("ROLE_USER")
     @GetMapping("/{pantryId}/pantry/search")
     public List<ItemDetailDto> searchItemsInPantry(@PathVariable long pantryId, PantrySearchDto searchParams) {
         LOGGER.info("GET /api/v1/group/{}/pantry", pantryId);
