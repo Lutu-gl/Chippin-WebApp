79e79eaaa32d2f70941e94adee63ce102e614f99
Rafael Milchram <e12216445@student.tuwien.ac.at>
2024-05-06 00:50:09 +0200

    Add validation to itemDetailDto

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/ItemDetailDto.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/ItemDetailDto.java
@@ -1,6 +1,10 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint.dto;
 
 import at.ac.tuwien.sepr.groupphase.backend.entity.Unit;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,7 +13,12 @@ import lombok.Setter;
 @Setter
 @Builder
 public class ItemDetailDto {
+    @NotNull
+    @NotBlank
+    @Size(min = 2, max = 60)
     private String description;
+    @PositiveOrZero
     private double amount;
+    @NotNull
     private Unit unit;
 }
