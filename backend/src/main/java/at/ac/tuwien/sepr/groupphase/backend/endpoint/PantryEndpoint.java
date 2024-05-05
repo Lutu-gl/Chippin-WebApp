0f023f169ec48fe97d6d7a1be5d10ad8391c5922
Rafael Milchram <e12216445@student.tuwien.ac.at>
2024-05-06 00:49:27 +0200

    Add pantry post mapping

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/PantryEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/PantryEndpoint.java
@@ -1,21 +1,29 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint;
 
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.ItemDetailDto;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.ItemDto;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.PantryDetailDto;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.PantrySearchDto;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.mapper.ItemMapper;
+import at.ac.tuwien.sepr.groupphase.backend.entity.Item;
 import at.ac.tuwien.sepr.groupphase.backend.service.PantryService;
+import jakarta.validation.Valid;
+import jakarta.xml.bind.ValidationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.invoke.MethodHandles;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/v1/group")
@@ -41,7 +49,16 @@ public class PantryEndpoint {
     @GetMapping("/{pantryId}/pantry/search")
     public PantryDetailDto searchItemsInPantry(@PathVariable long pantryId, PantrySearchDto searchParams) {
         LOGGER.info("GET /api/v1/group/{}/pantry/search", pantryId);
-        LOGGER.debug("request parameters: {}", searchParams.getDetails());
+        LOGGER.debug("request parameters: {}", searchParams);
         return new PantryDetailDto(itemMapper.listOfItemsToListOfItemDetailDto(pantryService.findItemsByDescription(searchParams.getDetails(), pantryId)));
+    }
+
+    @Secured("ROLE_USER")
+    @PostMapping("/{pantryId}/pantry")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ItemDto addItemToPantry(@PathVariable long pantryId, @Valid @RequestBody ItemDetailDto itemDetailDto) throws ValidationException {
+        LOGGER.info("POST /api/v1/group/{}/pantry body: {}", pantryId, itemDetailDto);
+        Item item = itemMapper.itemDetailDtoToItem(itemDetailDto);
+        return itemMapper.itemToItemDto(pantryService.addItemToPantry(item, pantryId));
     }
 }
