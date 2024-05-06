928e551e0cf5e567f7b166ad203aba1674f31056
Rafael Milchram <e12216445@student.tuwien.ac.at>
2024-05-06 13:49:44 +0200

    Add delete mapping in pantry endpoint

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/PantryEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/PantryEndpoint.java
@@ -8,20 +8,20 @@ import at.ac.tuwien.sepr.groupphase.backend.endpoint.mapper.ItemMapper;
 import at.ac.tuwien.sepr.groupphase.backend.entity.Item;
 import at.ac.tuwien.sepr.groupphase.backend.service.PantryService;
 import jakarta.validation.Valid;
-import jakarta.xml.bind.ValidationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.invoke.MethodHandles;
 
@@ -42,7 +42,7 @@ public class PantryEndpoint {
     @GetMapping("/{pantryId}/pantry")
     public PantryDetailDto findAllInPantry(@PathVariable long pantryId) {
         LOGGER.info("GET /api/v1/group/{}/pantry", pantryId);
-        return new PantryDetailDto(itemMapper.listOfItemsToListOfItemCreateDto(pantryService.findAllItems(pantryId)));
+        return new PantryDetailDto(itemMapper.listOfItemsToListOfItemDto(pantryService.findAllItems(pantryId)));
     }
 
     @Secured("ROLE_USER")
@@ -50,15 +50,23 @@ public class PantryEndpoint {
     public PantryDetailDto searchItemsInPantry(@PathVariable long pantryId, PantrySearchDto searchParams) {
         LOGGER.info("GET /api/v1/group/{}/pantry/search", pantryId);
         LOGGER.debug("request parameters: {}", searchParams);
-        return new PantryDetailDto(itemMapper.listOfItemsToListOfItemCreateDto(pantryService.findItemsByDescription(searchParams.getDetails(), pantryId)));
+        return new PantryDetailDto(itemMapper.listOfItemsToListOfItemDto(pantryService.findItemsByDescription(searchParams.getDetails(), pantryId)));
     }
 
     @Secured("ROLE_USER")
     @PostMapping("/{pantryId}/pantry")
     @ResponseStatus(HttpStatus.CREATED)
-    public ItemDto addItemToPantry(@PathVariable long pantryId, @Valid @RequestBody ItemCreateDto itemCreateDto) throws ValidationException {
+    public ItemDto addItemToPantry(@PathVariable long pantryId, @Valid @RequestBody ItemCreateDto itemCreateDto) {
         LOGGER.info("POST /api/v1/group/{}/pantry body: {}", pantryId, itemCreateDto);
         Item item = itemMapper.itemCreateDtoToItem(itemCreateDto);
         return itemMapper.itemToItemDto(pantryService.addItemToPantry(item, pantryId));
+    }
+
+    @Secured("ROLE_USER")
+    @DeleteMapping("/{pantryId}/pantry/{itemId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteItem(@PathVariable long pantryId, @PathVariable long itemId) {
+        LOGGER.info("DELETE /api/v1/group/{}/pantry/{}", pantryId, itemId);
+        pantryService.deleteItem(pantryId, itemId);
     }
 }
