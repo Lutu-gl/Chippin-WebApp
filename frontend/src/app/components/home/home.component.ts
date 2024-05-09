e58ec16e726cfa84cdb28faf730a4f99fc01cdf2
Rafael Milchram <e12216445@student.tuwien.ac.at>
2024-05-09 20:49:18 +0200

    Add cards for groups, shopping lists, expenses and data on home page

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
+import {GroupDto} from "../../dtos/group";
+import {GroupService} from "../../services/group.service";
 
 @Component({
   selector: 'app-home',
@@ -8,9 +10,27 @@ import {AuthService} from '../../services/auth.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(public authService: AuthService) { }
-
-  ngOnInit() {
+  constructor(
+    public authService: AuthService,
+    private groupService: GroupService) { }
+  groups: GroupDto[] = [];
+  ngOnInit(): void {
+    this.groupService.getGroups().subscribe({
+      next: data => {
+        this.groups = data;
+        console.log(this.groups);
+      },
+      error: error => {
+        if (error && error.error && error.error.errors) {
+          for (let i = 0; i < error.error.errors.length; i++) {
+            console.error(`${error.error.errors[i]}`); // TODO this.notification.error(`${error.error.errors[i]}`); ?
+          }
+        } else if (error && error.error && error.error.message) { // if no detailed error explanation exists. Give a more general one if available.
+          console.error(`${error.error.message}`) ; // TODO this.notification.error(`${error.error.message}`);
+        } else {
+          console.error('Error getting groups', error);
+        }
+      }
+    });
   }
-
 }
