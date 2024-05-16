bcb4c6c44d58f400ecc99937bdeaf6c5d176ce57
Rafael Milchram <e12216445@student.tuwien.ac.at>
2024-05-16 17:10:33 +0200

    Move recipe and item-list content on home page to other components

--- frontend/src/app/components/recipe/recipe-info-card-content/recipe-info-card-content.component.ts
+++ frontend/src/app/components/recipe/recipe-info-card-content/recipe-info-card-content.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgForOf, NgIf, SlicePipe} from "@angular/common";
+import {RecipeDetailDto} from "../../../dtos/recipe";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-recipe-info-card-content',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf,
+    SlicePipe,
+    RouterLink
+  ],
   templateUrl: './recipe-info-card-content.component.html',
   styleUrl: './recipe-info-card-content.component.scss'
 })
 export class RecipeInfoCardContentComponent {
-
+  recipes: RecipeDetailDto[] = [];
 }
