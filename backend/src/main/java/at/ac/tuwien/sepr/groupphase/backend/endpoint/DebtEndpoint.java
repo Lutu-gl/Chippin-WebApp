4fa875b6ae3792cdeed15472b6492e4fc81a09a8
Lutu-gl <74654393+Lutu-gl@users.noreply.github.com>
2024-05-21 12:16:30 +0200

    [Debt] Added calculation and layers of debt in the backend for groups

---
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/DebtEndpoint.java
@@ -0,0 +1,53 @@
+package at.ac.tuwien.sepr.groupphase.backend.endpoint;
+
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.ActivityDetailDto;
+import at.ac.tuwien.sepr.groupphase.backend.exception.NotFoundException;
+import at.ac.tuwien.sepr.groupphase.backend.service.ActivityService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.invoke.MethodHandles;
+import java.util.Collection;
+
+@RestController
+@RequestMapping(value = "/api/v1/activity")
+public class DeptEndpoint {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    ActivityService activityService;
+
+    public DeptEndpoint(ActivityService activityService) {
+        this.activityService = activityService;
+    }
+
+    @Secured("ROLE_USER")
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("{id}")
+    public ActivityDetailDto getById(@PathVariable("id") long id) throws NotFoundException { // TODO check if this correct
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        ActivityDetailDto res = null;
+        res = activityService.getById(id);
+
+        return res;
+    }
+
+    @Secured("ROLE_USER")
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/group-expenses/{id}")
+    public Collection<ActivityDetailDto> getGroupExpenses(@PathVariable("id") long groupId) throws NotFoundException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return activityService.getExpenseActivitiesByGroupId(groupId, authentication.getName());
+    }
+
+}
---
+++ backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/unittests/servicetests/DebtServiceTest.java
@@ -0,0 +1,124 @@
+package at.ac.tuwien.sepr.groupphase.backend.unittests.servicetests;
+
+import at.ac.tuwien.sepr.groupphase.backend.basetest.BaseTest;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.shoppinglist.ShoppingListCreateDto;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.mapper.ShoppingListMapperImpl;
+import at.ac.tuwien.sepr.groupphase.backend.entity.ApplicationUser;
+import at.ac.tuwien.sepr.groupphase.backend.entity.GroupEntity;
+import at.ac.tuwien.sepr.groupphase.backend.entity.ShoppingList;
+import at.ac.tuwien.sepr.groupphase.backend.exception.NotFoundException;
+import at.ac.tuwien.sepr.groupphase.backend.repository.GroupRepository;
+import at.ac.tuwien.sepr.groupphase.backend.repository.ShoppingListRepository;
+import at.ac.tuwien.sepr.groupphase.backend.repository.UserRepository;
+import at.ac.tuwien.sepr.groupphase.backend.service.impl.ShoppingListServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ShoppingListServiceTest extends BaseTest {
+
+    @Mock
+    private ShoppingListRepository shoppingListRepository;
+
+    @Spy
+    private ShoppingListMapperImpl shoppingListMapper;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private GroupRepository groupRepository;
+
+    @InjectMocks
+    private ShoppingListServiceImpl shoppingListService;
+
+    @Test
+    public void givenValidShoppingListCreateDtoWithoutGroupId_whenCreateShoppingList_thenNoException() {
+        when(shoppingListRepository.save(any())).thenReturn(new ShoppingList());
+        when(groupRepository.findById(any())).thenReturn(Optional.of(GroupEntity.builder().id(-1L).build()));
+        when(userRepository.findById(any())).thenReturn(Optional.of(ApplicationUser.builder().id(-1L).build()));
+        var shoppingListCreateDto = ShoppingListCreateDto.builder()
+            .name("Test Shopping List")
+            .build();
+
+        shoppingListService.createShoppingList(shoppingListCreateDto, -1L);
+
+        verify(shoppingListRepository, times(1)).save(any());
+    }
+
+    @Test
+    public void givenValidShoppingListCreateDtoWithValidGroupId_whenCreateShoppingList_thenNoException() {
+        when(shoppingListRepository.save(any())).thenReturn(new ShoppingList());
+        when(groupRepository.findById(any())).thenReturn(Optional.of(GroupEntity.builder().id(-1L).build()));
+        when(userRepository.findById(any())).thenReturn(Optional.of(ApplicationUser.builder().id(-1L).build()));
+        var shoppingListCreateDto = ShoppingListCreateDto.builder()
+            .name("Test Shopping List")
+            .groupId(-1L)
+            .build();
+
+        shoppingListService.createShoppingList(shoppingListCreateDto, -1L);
+
+        verify(shoppingListRepository, times(1)).save(any());
+    }
+
+    @Test
+    public void givenValidShoppingListCreateDtoWithInvalidGroupId_whenCreateShoppingList_thenNotFoundException() {
+        when(groupRepository.findById(any())).thenReturn(Optional.empty());
+        when(userRepository.findById(any())).thenReturn(Optional.of(ApplicationUser.builder().id(-1L).build()));
+        var shoppingListCreateDto = ShoppingListCreateDto.builder()
+            .name("Test Shopping List")
+            .groupId(-1L)
+            .build();
+        assertThrows(NotFoundException.class, () -> shoppingListService.createShoppingList(shoppingListCreateDto, -1L));
+
+        verify(shoppingListRepository, times(0)).save(any());
+    }
+
+    @Test
+    public void givenValidShoppingListId_whenGetShoppingList_thenNoException() {
+        when(shoppingListRepository.findById(any())).thenReturn(Optional.of(new ShoppingList()));
+
+        shoppingListService.getShoppingList(-1L);
+
+        verify(shoppingListRepository, times(1)).findById(any());
+    }
+
+    @Test
+    public void givenValidShoppingListId_whenDeleteShoppingList_thenNoException() {
+        shoppingListService.deleteShoppingList(-1L);
+
+        verify(shoppingListRepository, times(1)).deleteById(any());
+    }
+
+    @Test
+    public void givenValidGroupId_whenGetShoppingListsForGroup_thenNoException() {
+        when(shoppingListRepository.findAllByGroupId(any())).thenReturn(List.of());
+
+        shoppingListService.getShoppingListsForGroup(-1L);
+
+        verify(shoppingListRepository, times(1)).findAllByGroupId(any());
+    }
+
+    @Test
+    public void givenValidUserId_whenGetShoppingListsForUser_thenNoException() {
+        when(shoppingListRepository.findAllByOwnerId(any())).thenReturn(List.of());
+
+        shoppingListService.getShoppingListsForUser(-1L);
+
+        verify(shoppingListRepository, times(1)).findAllByOwnerId(any());
+    }
+}
