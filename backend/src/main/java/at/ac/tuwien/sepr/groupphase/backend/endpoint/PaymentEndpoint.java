7d3a1e30c6a9d7d1cc86bbb34b05cf7e02642157
Lutu-gl <74654393+Lutu-gl@users.noreply.github.com>
2024-05-22 13:52:57 +0200

    [Payment] Added update, delete, get and recover to payments

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/PaymentEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/PaymentEndpoint.java
@@ -12,7 +12,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -28,6 +32,14 @@ public class PaymentEndpoint {
     private final PaymentService paymentService;
 
     @Secured("ROLE_USER")
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{id}")
+    public PaymentDto getById(@PathVariable long id) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return paymentService.getById(id, authentication.getName());
+    }
+
+    @Secured("ROLE_USER")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public PaymentDto createPayment(@Valid @RequestBody PaymentDto paymentDto) throws ValidationException, ConflictException {
@@ -35,4 +47,27 @@ public class PaymentEndpoint {
         return paymentService.createPayment(paymentDto, authentication.getName());
     }
 
+    @Secured("ROLE_USER")
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/{id}")
+    public PaymentDto updatePayment(@PathVariable(name = "id") Long paymentId, @Valid @RequestBody PaymentDto paymentDto) throws ValidationException, ConflictException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return paymentService.updatePayment(paymentId, paymentDto, authentication.getName());
+    }
+
+    @Secured("ROLE_USER")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deletePayment(@PathVariable(name = "id") Long paymentId) throws ConflictException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        paymentService.deletePayment(paymentId, authentication.getName());
+    }
+
+    @Secured("ROLE_USER")
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/recover/{id}")
+    public PaymentDto recoverPayment(@PathVariable(name = "id") Long paymentId) throws ConflictException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return paymentService.recoverPayment(paymentId, authentication.getName());
+    }
 }
