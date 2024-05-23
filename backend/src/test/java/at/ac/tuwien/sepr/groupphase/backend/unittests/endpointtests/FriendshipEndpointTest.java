a492e149bf6c33222414993b0be73e727d49359e
Lukas Schatzer <61950131+zLuki@users.noreply.github.com>
2024-05-23 21:32:19 +0200

    Improved test case coverage

--- backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/unittests/endpointtests/FriendshipEndpointTest.java
+++ backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/unittests/endpointtests/FriendshipEndpointTest.java
@@ -22,11 +22,13 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Collection;
 import java.util.List;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -41,7 +43,7 @@ public class FriendshipEndpointTest extends BaseTest {
     private UserService userService;
     @MockBean
     private FriendshipService friendshipService;
-    @Autowired
+    @MockBean
     private FriendshipRepository friendshipRepository;
     @Autowired
     private ObjectMapper objectMapper;
@@ -50,29 +52,8 @@ public class FriendshipEndpointTest extends BaseTest {
     @Autowired
     private SecurityProperties securityProperties;
 
-    private static String TEST_EMAIL_1 = "friendshipTestUser1@test.com";
-    private static String TEST_EMAIL_2 = "friendshipTestUser2@test.com";
-
-    /*
-    @BeforeEach
-    public void registerTestUser() {
-        friendshipRepository.deleteAll();
-        UserRegisterDto userRegisterDto1 = UserRegisterDto.builder()
-            .email(TEST_EMAIL_1)
-            .password("Password0")
-            .build();
-
-        UserRegisterDto userRegisterDto2 = UserRegisterDto.builder()
-            .email(TEST_EMAIL_2)
-            .password("Password0")
-            .build();
-
-        try {
-            userService.register(userRegisterDto1, false);
-            userService.register(userRegisterDto2, false);
-        } catch (UserAlreadyExistsException ignored) {
-        }
-    }*/
+    private static final String TEST_EMAIL_1 = "friendshipTestUser1@test.com";
+    private static final String TEST_EMAIL_2 = "friendshipTestUser2@test.com";
 
     private String[] getLoginTokensOfTestUsers() {
         String bearerToken1 = jwtTokenizer.getAuthToken(TEST_EMAIL_1, List.of("ROLE_USER"));
@@ -130,4 +111,44 @@ public class FriendshipEndpointTest extends BaseTest {
             .andExpect(status().isOk());
     }
 
+    @Test
+    public void testGetFriendsShouldReturn200() throws Exception {
+        String[] tokens = getLoginTokensOfTestUsers();
+
+        friendshipService.sendFriendRequest(TEST_EMAIL_2, TEST_EMAIL_1);
+        friendshipService.acceptFriendRequest(TEST_EMAIL_2, TEST_EMAIL_1);
+
+        when(friendshipService.getFriends(TEST_EMAIL_1)).thenReturn(List.of(TEST_EMAIL_2));
+
+        byte[] body = mockMvc.perform(get("/api/v1/friendship/friends")
+                .header(securityProperties.getAuthHeader(), tokens[0])
+            )
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsByteArray();
+
+        Collection<String> res = objectMapper.readerFor(Collection.class).readValue(body);
+        assertEquals(1, res.size());
+        assertTrue(res.contains(TEST_EMAIL_2));
+    }
+
+    @Test
+    public void testGetIncomingFriendRequestsShouldReturn200() throws Exception {
+        String[] tokens = getLoginTokensOfTestUsers();
+        friendshipService.sendFriendRequest(TEST_EMAIL_2, TEST_EMAIL_1);
+        when(friendshipService.getIncomingFriendRequest(TEST_EMAIL_1)).thenReturn(List.of(TEST_EMAIL_2));
+
+        byte[] body = mockMvc.perform(get("/api/v1/friendship/friend-requests")
+                .header(securityProperties.getAuthHeader(), tokens[0])
+            )
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsByteArray();
+
+        Collection<String> res = objectMapper.readerFor(Collection.class).readValue(body);
+        assertEquals(1, res.size());
+        assertTrue(res.contains(TEST_EMAIL_2));
+    }
 }
