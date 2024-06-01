ead9d81e2dadcabeab79c4d25d9e1e9902c3fe7b
Lukas Schatzer <Schatzer.Lukas@gmail.com>
2024-06-01 13:25:40 +0200

    [frontend] Working on new group page

--- frontend/src/app/components/group-list/group-info/group-info.component.ts
+++ frontend/src/app/components/group-list/group-info/group-info.component.ts
@@ -1,12 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {GroupService} from "../../../services/group.service";
-import {ToastrService} from "ngx-toastr";
-import {GroupDto} from "../../../dtos/group";
-import { BudgetDto } from '../../../dtos/budget';
-import {ActivatedRoute, Router} from "@angular/router";
-import {DebtGroupDetailDto} from "../../../dtos/debt";
-import {DebtService} from "../../../services/debt.service";
-import {ActivityType} from "../../expense/expense-list.component";
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-group-info',
@@ -14,58 +7,146 @@ import {ActivityType} from "../../expense/expense-list.component";
   styleUrl: './group-info.component.scss'
 })
 export class GroupInfoComponent implements OnInit {
+  
+  data: any;
+  options: any;
+  tabMenuItems: MenuItem[] | undefined;
+  tabMenuActiveItem: MenuItem | undefined;
 
-  group: GroupDto = {
-    groupName: '',
-    members: []
-  };
+  constructor(){
 
-  debt: DebtGroupDetailDto
-
-  constructor(
-    private service: GroupService,
-    private debtService: DebtService,
-    private router: Router,
-    private route: ActivatedRoute,
-    private groupService: GroupService,
-    private notification: ToastrService,
-  ) {
   }
 
   ngOnInit(): void {
-    this.getGroup();
-    this.getDebt();
+
+    // tab Menu
+    this.tabMenuItems = [
+      { label: 'Transactions', icon: 'pi pi-fw pi-dollar' },
+      { label: 'Debts', icon: 'pi pi-fw pi-money-bill' },
+      { label: 'Members', icon: 'pi pi-fw pi-users' },
+      { label: 'Pantry', icon: 'pi pi-fw pi-shopping-cart' },
+      { label: 'Shopping lists', icon: 'pi pi-fw pi-shopping-cart' }
+    ];
+    this.tabMenuActiveItem = this.tabMenuItems[0];
+
+
+    // TODO: change this data
+    this.data = {
+      labels: ['Emil Hafner', 'Rafael Milchram', 'Markus Berling'],
+      datasets: [
+        {
+          label: 'Total Amount',
+          backgroundColor: ['#42A5F5', '#66BB6A', '#FFA726'],
+          data: [-29.99, 12.99, 17.00]
+        }
+      ]
+    };
+
+    this.options = {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    };
   }
 
-  getGroup(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.service.getById(id)
-      .subscribe(pGroup => {
-        this.group = pGroup;
-      });
+  onActiveItemChange(event: MenuItem) {
+    this.tabMenuActiveItem = event;
   }
 
-  getDebt(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.debtService.getDebtById(id)
-      .subscribe(debt => {
-        this.debt = debt;
-        console.log(debt);
-      });
+  isTransactionsSelected(): boolean {
+    return this.tabMenuActiveItem === this.tabMenuItems[0];
   }
 
-  objectKeys(obj: any): string[] {
-    return Object.keys(obj).sort();
+  isDebtsSelected(): boolean {
+    return this.tabMenuActiveItem === this.tabMenuItems[1];
   }
 
-  getSortedMembers(): any[] {
-    return this.group.members.sort((a, b) => a.localeCompare(b));
+  isMembersSelected(): boolean {
+    return this.tabMenuActiveItem === this.tabMenuItems[2];
   }
 
-  getBorderColor(value: number): string {
-    return value > 0 ? 'green' : 'red';
+  isPantrySelected(): boolean {
+    return this.tabMenuActiveItem === this.tabMenuItems[3];
   }
 
-    protected readonly ActivityType = ActivityType;
-  protected readonly parseFloat = parseFloat;
+  isShoppingListsSelected(): boolean {
+    return this.tabMenuActiveItem === this.tabMenuItems[4];
+  }
+
 }
+
+// import {Component, OnInit} from '@angular/core';
+// import {GroupService} from "../../../services/group.service";
+// import {ToastrService} from "ngx-toastr";
+// import {GroupDto} from "../../../dtos/group";
+// import { BudgetDto } from '../../../dtos/budget';
+// import {ActivatedRoute, Router} from "@angular/router";
+// import {DebtGroupDetailDto} from "../../../dtos/debt";
+// import {DebtService} from "../../../services/debt.service";
+// import {ActivityType} from "../../expense/expense-list.component";
+
+// @Component({
+//   selector: 'app-group-info',
+//   templateUrl: './group-info.component.html',
+//   styleUrl: './group-info.component.scss'
+// })
+// export class GroupInfoComponent implements OnInit {
+
+//   group: GroupDto = {
+//     groupName: '',
+//     members: []
+//   };
+
+//   debt: DebtGroupDetailDto
+
+//   constructor(
+//     private service: GroupService,
+//     private debtService: DebtService,
+//     private router: Router,
+//     private route: ActivatedRoute,
+//     private groupService: GroupService,
+//     private notification: ToastrService,
+//   ) {
+//   }
+
+//   ngOnInit(): void {
+//     this.getGroup();
+//     this.getDebt();
+//   }
+
+//   getGroup(): void {
+//     const id = Number(this.route.snapshot.paramMap.get('id'));
+//     this.service.getById(id)
+//       .subscribe(pGroup => {
+//         this.group = pGroup;
+//       });
+//   }
+
+//   getDebt(): void {
+//     const id = Number(this.route.snapshot.paramMap.get('id'));
+//     this.debtService.getDebtById(id)
+//       .subscribe(debt => {
+//         this.debt = debt;
+//         console.log(debt);
+//       });
+//   }
+
+//   objectKeys(obj: any): string[] {
+//     return Object.keys(obj).sort();
+//   }
+
+//   getSortedMembers(): any[] {
+//     return this.group.members.sort((a, b) => a.localeCompare(b));
+//   }
+
+//   getBorderColor(value: number): string {
+//     return value > 0 ? 'green' : 'red';
+//   }
+
+//     protected readonly ActivityType = ActivityType;
+//   protected readonly parseFloat = parseFloat;
+// }
